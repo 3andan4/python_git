78a0d6e8064d54f1ebd226833f45cc13390f753e
Maxime <maxime.le-besnerais@epitech.eu>
2023-07-29 00:21:06 +0200

    [SRC] 1 file changed 	Files: 		sources/: Untracked
    	Comment:
    		Commit made without comment

---
+++ sources/main.cpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2023
+** giti
+** File description:
+** main.cpp
+*/
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
