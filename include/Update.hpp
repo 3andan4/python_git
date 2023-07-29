eff0a85a88fcd7473e78bcf964fbe4c5f1574aac
Maxime <maxime.le-besnerais@epitech.eu>
2023-07-29 18:54:20 +0200

    [WIP] Adds Update related files & definitions 	Files: 		include/Update.hpp: Untracked 		src/FetchUpdate.cpp: Untracked
    	Comment:
    		Makes Versions, Date & changelog into classes for ease of comparison.

---
+++ include/Update.hpp
@@ -0,0 +1,136 @@
+/*
+** EPITECH PROJECT, 2023
+** giti
+** File description:
+** Update.hpp
+*/
+
+#ifndef GITI_UPDATE_HPP
+    #define GITI_UPDATE_HPP
+    #include "lib.pch"
+    #include "GitiExceptions.hpp"
+    class Date {
+        public:
+        Date(std::string);
+        Date(int day, int month, int year) : day(day), month(month), year(year) {};
+        Date() = default;
+        ~Date() = default;
+        bool operator==(const Date &rhs) const {
+            return day == rhs.day &&
+                   month == rhs.month &&
+                   year == rhs.year;
+        }
+        bool operator!=(const Date &rhs) const {
+            return !(rhs == *this);
+        }
+        bool operator<(const Date &rhs) const {
+            if (day < rhs.day)
+                return true;
+            if (rhs.day < day)
+                return false;
+            if (month < rhs.month)
+                return true;
+            if (rhs.month < month)
+                return false;
+            return year < rhs.year;
+        }
+        bool operator>(const Date &rhs) const {
+            return rhs < *this;
+        }
+        std::string __str__() const {
+            return std::to_string(day) + "/" + std::to_string(month) + "/" + std::to_string(year);
+        }
+        int dayDiff(const Date &rhs) const {
+            int total_days = day + month * 30 + year * 365;
+            int total_days_rhs = rhs.day + rhs.month * 30 + rhs.year * 365;
+            return total_days - total_days_rhs;
+        }
+        int day;
+        int month;
+        int year;
+    };
+    class Version {
+        public:
+        Version(std::string);
+        Version() = default;
+        ~Version() = default;
+        bool operator==(const Version &rhs) const { // equivalent of SameVersion
+            return build == rhs.build &&
+                   major == rhs.major &&
+                   minor == rhs.minor &&
+                   patch == rhs.patch;
+        }
+        bool operator!=(const Version &rhs) const { // equivalent of DifferentVersion
+            return !(rhs == *this);
+        }
+        bool operator<(const Version &rhs) const { // equivalent of OlderVersion
+            if (build < rhs.build)
+                return true;
+            if (rhs.build < build)
+                return false;
+            if (major < rhs.major)
+                return true;
+            if (rhs.major < major)
+                return false;
+            if (minor < rhs.minor)
+                return true;
+            if (rhs.minor < minor)
+                return false;
+            return patch < rhs.patch;
+        }
+        bool operator>(const Version &rhs) const { // equivalent of NewerVersion
+            return rhs < *this;
+        }
+        std::string __str__() const {
+            return std::to_string(build) + "." + std::to_string(major) + "." + std::to_string(minor) + "." + std::to_string(patch);
+        }
+        int build;
+        int major;
+        int minor;
+        int patch;
+    };
+    /**
+     * @brief Class representing a changelog - It will parse the changelog file and store the data in a JSON object
+     */
+    class Changelog {
+    public:
+        Changelog(std::string filename, bool is_file);
+        ~Changelog() = default;
+        void display();
+    protected:
+    rapidjson::Document _data;
+    Version _currentVersion;
+    Date _lastUpdateDate;
+    Date _today;
+    Date _LUCDate;
+    int _updatedSince;
+    };
+
+    class Update {
+    public:
+        Update() = default;
+        ~Update() = default;
+        /**
+         * @brief Fetch the remote changelog file to compare it with the local one
+         * @warning This function will throw an exception if it can't fetch the remote changelog file
+         */
+        void fetchChangelog();
+        /**
+         * @brief Parse the remote changelog file
+         * @warning This function will throw an exception if it can't parse the remote changelog file
+         */
+        void parseChangelog();
+        /**
+         * @brief Compare the local and remote changelog files to determine if an update is available
+         * @warning This function will throw an exception if it can't compare the local and remote changelog files
+         */
+        void compareVersions();
+        /**
+         * @brief Update the local installation
+         * @warning This function will throw an exception if it can't update the local installation
+         */
+        void update();
+        Changelog _localChangelog;
+        Changelog _remoteChangelog;
+    };
+#endif //GITI_UPDATE_HPP
---
+++ src/FetchUpdate.cpp
@@ -0,0 +1,93 @@
+/*
+** EPITECH PROJECT, 2023
+** giti
+** File description:
+** FetchUpdate.cpp
+*/
+
+// https://raw.githubusercontent.com/MaximeLeBesnerais/giti_super/main/versions_changelog.json
+
+// installation path: /home/$USER/.giti/versions_changelog.json
+
+#include "Update.hpp"
+
+int daysSince(const std::string& dateStr) {
+    std::time_t t = std::time(0);
+    std::tm* today = std::localtime(&t);
+
+    std::tm inputDate = {};
+    std::istringstream dateStream(dateStr);
+    dateStream >> std::get_time(&inputDate, "%d/%m/%Y");
+
+    if (dateStream.fail()) {
+        throw std::runtime_error("Failed to parse date string");
+        return -1;
+    }
+
+    std::time_t t1 = std::mktime(today);
+    std::time_t t2 = std::mktime(&inputDate);
+
+    double difference = std::difftime(t2, t1);
+
+    int daysDiff = static_cast<int>(difference / (60 * 60 * 24));
+
+    return daysDiff;
+}
+
+Version::Version(std::string vers) {
+    std::vector<std::string> splitted;
+    std::stringstream ss(vers);
+    std::string token;
+    while (std::getline(ss, token, '.'))
+        splitted.push_back(token);
+    if (splitted.size() != 4)
+        throw GitiException(2);
+    this->build = std::stoi(splitted[0]);
+    this->major = std::stoi(splitted[1]);
+    this->minor = std::stoi(splitted[2]);
+    this->patch = std::stoi(splitted[3]);
+}
+
+Date::Date(std::string date) {
+    std::vector<std::string> splitted;
+    std::stringstream ss(date);
+    std::string token;
+    while (std::getline(ss, token, '/'))
+        splitted.push_back(token);
+    if (splitted.size() != 3)
+        throw GitiException(2);
+    this->day = std::stoi(splitted[0]);
+    this->month = std::stoi(splitted[1]);
+    this->year = std::stoi(splitted[2]);
+}
+
+Changelog::Changelog(std::string d_cont, bool is_file = true)
+{
+    std::string content;
+    if (!is_file)
+        content = d_cont;
+    else {
+        std::ifstream ifs(d_cont);
+        if (!ifs.is_open())
+            throw GitiException(3);
+        content = std::string((std::istreambuf_iterator<char>(ifs)), std::istreambuf_iterator<char>());
+        ifs.close();
+    }
+    _data.Parse(content.c_str());
+    if (_data.HasParseError())
+        throw GitiException(4);
+    _currentVersion = Version(_data["version"].GetString());
+    _lastUpdateDate = Date(_data["date"].GetString());
+    _LUCDate = Date(_data["LUC"].GetString());
+    std::time_t t = std::time(0);
+    std::tm* today = std::localtime(&t);
+    _today = Date(today->tm_mday, today->tm_mon + 1, today->tm_year + 1900);
+    _updatedSince = _today.dayDiff(_LUCDate);
+}
+
+void Changelog::display() {
+    std::cout << "Current version: " << _currentVersion.__str__() << std::endl;
+    std::cout << "Last update: " << _lastUpdateDate.__str__() << std::endl;
+    std::cout << "Last update check: " << _LUCDate.__str__() << std::endl;
+    std::cout << "Updated since last check: " << _updatedSince << " days" << std::endl;
+}
