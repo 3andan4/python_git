9d0cbac86b3882fa8be0b0fefaa245258c877ed5
Maxime <maxime.le-besnerais@epitech.eu>
2023-07-29 18:54:46 +0200

    [EXCEPTIONS] Adds new codes 	Files: 		src/GitiExceptions.cpp: Modified
    	Comment:
    		Commit made without comment

--- src/GitiExceptions.cpp
+++ src/GitiExceptions.cpp
@@ -7,9 +7,12 @@
 
 #include "GitiExceptions.hpp"
 
-GitiException::GitiException(int code) {
+GitiException::GitiException(int code) noexcept {
     std::unordered_map<int, std::string> _errors = {
-        {1, "GitiException: Not a git repository (or any of the parent directories): .git"},
+        {1, "GitiException: Not a git repository (Giti must be run in a git repository)"},
+        {2, "GitiException: Parsed version number was invalid or Date parsing failed"},
+        {3, "GitiException: Can't open Local changelog - The file may have been deleted"},
+        {4, "GitiException: Can't parse Local changelog - The file may have been modified"}
     };
     auto it = _errors.find(code);
     if (it != _errors.end())
