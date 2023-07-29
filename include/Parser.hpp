ba65d2ea4e703022d70c0b6a51daba2999f10a93
Maxime <maxime.le-besnerais@epitech.eu>
2023-07-29 18:53:20 +0200

    [PARSER] Introduce parser (not used) 	Files: 		include/Parser.hpp: Modified
    	Comment:
    		Commit made without comment

--- include/Parser.hpp
+++ include/Parser.hpp
@@ -3,19 +3,18 @@
 //
 
 #ifndef GITI_PARSER_HPP
-#define GITI_PARSER_HPP
-#include "lib.pch"
-namespace po = boost::program_options;
+    #define GITI_PARSER_HPP
+    #include "lib.pch"
+    namespace po = boost::program_options;
 
 
-class Parser {
-public:
-    Parser() = default;
-    ~Parser() = default;
-    void parse(int ac, char **av);
+    class Parser {
+    public:
+        Parser() = default;
+        ~Parser() = default;
+        void parse(int ac, char **av);
 
-private:
-};
-
+    private:
+    };
 
 #endif //GITI_PARSER_HPP
