ed3729bd1d967dba1ce323668256033aecdbed15
Maxime <maxime.le-besnerais@epitech.eu>
2023-07-31 00:46:03 +0200

    [PARSER] Refactors the parser in multiple units 	Files: 		include/Parser.hpp: Modified 		src/Parser.cpp: Modified
    	Comment:
    		The parser now possesses 3 units. The 1st unit (regular unit) has 2 required arguments. The 2 other units work togethers for utility & tool options. This allows the unit to check better for conflicts.

--- include/Parser.hpp
+++ include/Parser.hpp
@@ -5,6 +5,8 @@
 #ifndef GITI_PARSER_HPP
     #define GITI_PARSER_HPP
     #include "lib.pch"
+    #include "StaticVar.hpp"
+    #include "GitiExceptions.hpp"
     namespace po = boost::program_options;
 
 
@@ -13,8 +15,16 @@
         Parser() = default;
         ~Parser() = default;
         void parse(int ac, char **av);
-        std::unordered_map<std::string, bool> _options;
+        std::unordered_map<std::string, bool> _u_options;
+        std::unordered_map<std::string, bool> _t_options;
+        std::unordered_map<std::string, bool> _c_options;
         po::options_description _all_options;
+        bool _error = false;
+        bool _utility = false;
+        bool _tool = false;
+        bool _comments = false;
+        std::string _tag;
+        std::vector<std::string> _files;
     };
 
 #endif //GITI_PARSER_HPP
--- src/Parser.cpp
+++ src/Parser.cpp
@@ -5,82 +5,123 @@
 #include "Parser.hpp"
 
 void Parser::parse(int ac, char **av) {
-    _options = {
+    _u_options = {
             {"version",         false},
-            {"test",            false},
-            {"no-commit",       false},
-            {"no-title",        false},
-            {"no-title-commit", false},
-            {"all",             false},
-            {"makefile",        false},
-            {"header",          false},
             {"update",          false},
             {"force-update",    false},
             {"remove",          false},
             {"igit",            false},
-            {"del",             false},
             {"help",            false},
             {"error",           false}
     };
-    po::options_description _desc("Giti Interactive Commit Tool", 120);
-    po::options_description _utility_options("Utility options");
-    _utility_options.add_options()
+    _t_options = {
+            {"all",      false},
+            {"del",      false},
+            {"makefile", false},
+            {"header",   false}
+    };
+    _c_options = {
+            {"title",   false},
+            {"message", false}
+    };
+
+    po::options_description _type1("Regular command");
+    po::options_description _optional("Optional arguments");
+    _optional.add_options()
+    ("no-commit,c", "Do not prompt for a commit comment")
+    ("no-title,t", "Do not prompt for a title")
+    ("no-title-commit,x", "Do not prompt for anything");
+    po::options_description _positional("Positional arguments");
+    _positional.add_options()
+    ("Tag", po::value<std::string>()->required(), "Tag of the commit")
+    ("Files", po::value<std::vector<std::string>>()->required(), "Files to commit");
+    _type1.add(_optional).add(_positional);
+
+    po::options_description _super("Super command");
+    po::options_description _type2("Utility commands");
+    _type2.add_options()
     ("update,u", "Update GITI if an update is available")
     ("force-update,F", "Remove current GITI version and reinstall the remote version")
     ("remove,R", "Remove GITI from your system")
     ("igit,i", "Commit .gitignore file(s)")
     ("help,h", "Display this help");
-    po::options_description _tool_options("Tool options");
-    _tool_options.add_options()
+    po::options_description _type3("Tool commands");
+    _type3.add_options()
     ("all,a", "Commit all modified files automatically")
     ("del,d", "Commit deleted file(s)")
     ("makefile,m", "Commit all Makefile & CMakeFile in the repo")
     ("header,H", "Commit all header files (.h, .hpp)");
-    po::options_description _comments_options("Comments options");
-    _comments_options.add_options()
-    ("no-commit,c", "Do not prompt for a commit comment")
-    ("no-title,t", "Do not prompt for a title")
-    ("no-title-commit,x", "Do not prompt for anything");
+    _super.add(_type2).add(_type3);
 
-    _desc.add(_utility_options).add(_tool_options).add(_comments_options);
-    _all_options.add(_desc);
+    _all_options.add(_type1).add(_super);
 
-    po::variables_map vm;
+    po::variables_map regular_vm;
+    po::variables_map type2_and_3_vm;
+
     try {
-        po::store(po::parse_command_line(ac, av, _all_options), vm);
-        po::notify(vm);
+        po::store(po::parse_command_line(ac, av, _super), type2_and_3_vm);
+        po::notify(type2_and_3_vm);
     } catch (std::exception &e) {
         std::cerr << "Error: " << e.what() << std::endl;
-        _options["error"] = true;
+        _error = true;
+    }
+
+    if (!_error) {
+        _u_options["version"] = (bool) type2_and_3_vm.count("version");
+        _u_options["update"] = (bool) type2_and_3_vm.count("update");
+        _u_options["force-update"] = (bool) type2_and_3_vm.count("force-update");
+        _u_options["remove"] = (bool) type2_and_3_vm.count("remove");
+        _u_options["igit"] = (bool) type2_and_3_vm.count("igit");
+        _u_options["help"] = (bool) type2_and_3_vm.count("help");
+        _t_options["all"] = (bool) type2_and_3_vm.count("all");
+        _t_options["del"] = (bool) type2_and_3_vm.count("del");
+        _t_options["makefile"] = (bool) type2_and_3_vm.count("makefile");
+        _t_options["header"] = (bool) type2_and_3_vm.count("header");
+
+        int count = std::count_if(_u_options.begin(), _u_options.end(), [](auto &option) { return option.second; });
+        int count2 = std::count_if(_t_options.begin(), _t_options.end(), [](auto &option) { return option.second; });
+
+        if (count > 1 || count2 > 1 || count > 0 && count2 > 0) {
+            _error = true;
+            return;
+        }
+
+        _utility = count > 0;
+        _tool = count2 > 0;
+
+        if (_utility && _tool) {
+            _error = true;
+            return;
+        }
+
+        if (_utility || _tool)
+            return;
+    }
+
+    _error = false;
+
+    try {
+        po::store(po::parse_command_line(ac, av, _type1), regular_vm);
+        po::notify(regular_vm);
+    } catch(const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        _error = true;
         return;
     }
-    _options["version"] = (bool) vm.count("version");
-    _options["test"] = (bool) vm.count("test");
-    _options["no-commit"] = (bool) vm.count("no-commit");
-    _options["no-title"] = (bool) vm.count("no-title");
-    _options["no-title-commit"] = (bool) vm.count("no-title-commit");
-    _options["all"] = (bool) vm.count("all");
-    _options["makefile"] = (bool) vm.count("makefile");
-    _options["header"] = (bool) vm.count("header");
-    _options["update"] = (bool) vm.count("update");
-    _options["force-update"] = (bool) vm.count("force-update");
-    _options["remove"] = (bool) vm.count("remove");
-    _options["igit"] = (bool) vm.count("igit");
-    _options["del"] = (bool) vm.count("del");
-    _options["help"] = (bool) vm.count("help");
 
-    // If there is more than one option, it's an error !
-    int count = 0;
-    for (auto &option : _options) {
-        if (option.second)
-            count++;
+    if (regular_vm.count("Tag") && regular_vm.count("Files")) {
+        _tag = regular_vm["Tag"].as<std::string>();
+        _files = regular_vm["Files"].as<std::vector<std::string>>();
+    } else {
+        _error = true;
+        return;
     }
-    if (count > 1) {
-        // set error to false
-        for (auto &option : _options)
-            if (option.second)
-                option.second = false;
-        _options["error"] = true;
-    }
+
+    _c_options["title"] = (bool) regular_vm.count("no-title") || (bool) regular_vm.count("no-title-commit");
+    _c_options["message"] = (bool) regular_vm.count("no-commit") || (bool) regular_vm.count("no-title-commit");
+
+    if (_c_options["title"] || _c_options["message"])
+        _comments = true;
+
     return;
 }
