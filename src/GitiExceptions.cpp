f66d18b97c9d005efa2c417acb3452c47351b049
Maxime <maxime.le-besnerais@epitech.eu>
2023-07-31 00:47:55 +0200

    [FIX] Adds some new errors for specific scenarios 	Files: 		src/GitiExceptions.cpp: Modified
    	Comment:
    		Commit made without comment

--- src/GitiExceptions.cpp
+++ src/GitiExceptions.cpp
@@ -12,7 +12,10 @@ GitiException::GitiException(int code) noexcept {
         {1, "GitiException: Not a git repository (Giti must be run in a git repository)"},
         {2, "GitiException: Parsed version number was invalid or Date parsing failed"},
         {3, "GitiException: Can't open Local changelog - The file may have been deleted"},
-        {4, "GitiException: Can't parse Local changelog - The file may have been modified"}
+        {4, "GitiException: Can't parse Local changelog - The file may have been modified"},
+        {5, "GitiException: You can't use utility options and tool options at the same time."},
+        {6, "GitiException: Error when exploring the git tree"},
+        {7, "GitiException: Error when trying to commit"},
     };
     auto it = _errors.find(code);
     if (it != _errors.end())
