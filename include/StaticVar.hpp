9db74b2b3c4ef42eb5e87a974b09ebeff17d2dda
Maxime <maxime.le-besnerais@epitech.eu>
2023-07-31 00:48:14 +0200

    [FIX] Changes Macros 	Files: 		include/StaticVar.hpp: Modified
    	Comment:
    		Commit made without comment

--- include/StaticVar.hpp
+++ include/StaticVar.hpp
@@ -11,6 +11,7 @@
 #ifndef GITI_STATICVAR_HPP
     #define GITI_STATICVAR_HPP
     #define JSON_LOCAL(home) home + "giti/versions_changelog.json"
-    #define JSON_LOCAL_DEBUG "/home/perso/giti/versions_changelog.json"
+    #define JSON_HOME JSON_LOCAL("/home" + std::string(getenv("USER")) + "/")
+    #define JSON_LOCAL_DEBUG JSON_LOCAL(std::string("/home/perso/"))
     #define JSON_REMOTE "https://raw.githubusercontent.com/MaximeLeBesnerais/giti_super/main/versions_changelog.json"
 #endif // GITI_STATICVAR_HPP
