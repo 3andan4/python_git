e984e0f2672d8b4114eba9018283dc518a7525a8
Maxime <maxime.le-besnerais@epitech.eu>
2024-01-19 10:32:48 +0100

    [FEAT] Add Data module with utility and tool data types

---
+++ src/Data.hs
@@ -0,0 +1,44 @@
+module Data (
+    Skip(..),
+    Utility(..),
+    Tool(..),
+    SpecialParser,
+    emptySpecialParser,
+    CommandLine(..)
+    ) where
+
+data Skip = Skip {
+  title :: Bool,
+  commit :: Bool
+} deriving (Show)
+
+
+-- SpecialParser
+
+data Utility = Utility {
+  allFiles :: Bool,
+  make :: Bool,
+  ignore :: Bool,
+  delete :: Bool,
+  header :: Bool
+} deriving (Show)
+
+data Tool = Tool {
+  version :: Bool,
+  update :: Bool,
+  forceUpdate :: Bool,
+  remove :: Bool,
+  help :: Bool
+} deriving (Show)
+
+data CommandLine = CommandLine {
+  skip :: Skip,
+  tag :: String,
+  files :: [String]
+} deriving (Show)
+
+type SpecialParser = (Utility, Tool)
+
+emptySpecialParser :: SpecialParser
+emptySpecialParser = (Utility False False False False False,
+    Tool False False False False False)
