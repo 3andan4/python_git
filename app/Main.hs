ed17a51626cd8bab0b41d949e72a875dadbcc482
Maxime <maxime.le-besnerais@epitech.eu>
2024-01-19 13:42:16 +0100

    Add test main

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,29 @@
 module Main (main) where
 
-import Lib
+import Data
+import System.Environment (getArgs)
+import Parser
+import GitOps (gitPorcelain, buildObjects)
+
+--  [["M", "app/Main.hs"], ["M", "src/Data.hs"]] => [("M", "app/Main.hs"), ("M", "src/Data.hs")]
+makeTupple :: [[String]] -> [(String, String)]
+makeTupple = map (\ x -> (head x, last x))
+
+tuppleAll :: [[String]] -> [(String, String)]
+tuppleAll [] = []
+tuppleAll list@(x:xs)
+    | length x == 2 = makeTupple list
+    | otherwise = tuppleAll xs
 
 main :: IO ()
-main = someFunc
+main = do
+    args <- getArgs
+    case commandLineParser args of
+        Left err -> putStrLn err
+        Right cmd -> putStrLn (show (skip cmd) ++ " " ++ show (tag cmd) ++ " " ++ show (files cmd))
+    porcelain <- gitPorcelain
+    let objs = buildObjects $ tuppleAll $ map (words . dropWhile (==' ')) (lines porcelain)
+    case objs of
+        Left err -> putStrLn err
+        Right items -> print items
+
